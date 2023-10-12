8d83e400d224ae2476a7cde8190fb1aa9de09411
Daniel Ruiz <danielruimar@gmail.com>
2023-10-12 22:27:21 +0200

    added protection to excel

--- src/domain/dto/excel.dto.ts
+++ src/domain/dto/excel.dto.ts
@@ -11,37 +11,77 @@ dotenv.config();
 const formatedDate = process.env.FORMAT_DATE_MOMENT as string;
 
 const createExcelWithUsers = async (): Promise<ExcelJS.Workbook> => {
-  const users = await userDto.getAllUser();
-  const workbook = new ExcelJS.Workbook();
-  const worksheet = workbook.addWorksheet("Usuarios");
+  try {
+    const users = await userDto.getAllUser();
+    const workbook = new ExcelJS.Workbook();
+    const worksheet = workbook.addWorksheet("Usuarios");
 
-  worksheet.columns = [
-    { header: "ID", key: "id", width: 10 },
-    { header: "Nombre", key: "nombre", width: 20 },
-    { header: "Apellido", key: "apellido", width: 20 },
-    { header: "Segundo Apellido", key: "segundo_apellido", width: 20 },
-    { header: "Teléfono", key: "telefono", width: 20 },
-    { header: "Email", key: "email", width: 20 },
-    { header: "DNI", key: "dni", width: 20 },
-    { header: "Fecha de Inscripción", key: "createdAt", width: 20 },
-  ];
+    worksheet.columns = [
+      { header: "ID", key: "id", width: 10 },
+      { header: "Nombre", key: "nombre", width: 20 },
+      { header: "Apellido", key: "apellido", width: 20 },
+      { header: "Segundo Apellido", key: "segundo_apellido", width: 20 },
+      { header: "Teléfono", key: "telefono", width: 20 },
+      { header: "Email", key: "email", width: 20 },
+      { header: "DNI", key: "dni", width: 20 },
+      { header: "Fecha de Inscripción", key: "createdAt", width: 20 },
+    ];
 
-  const formattedUsers = users.map((user) => ({
-    ...user,
-    nombre: user.nombre.toLocaleLowerCase(),
-    apellido: user.apellido.toLocaleLowerCase(),
-    segundo_apellido: user.segundo_apellido.toLocaleLowerCase(),
-    email: user.email.toLocaleLowerCase(),
-    dni: user.dni.toLocaleUpperCase(),
-    createdAt: format(user.createdAt, formatedDate, { locale: es }),
-  }));
+    worksheet.eachRow((row) => {
+      row.eachCell((cell) => {
+        cell.alignment = { vertical: "middle", horizontal: "center" };
+      });
+    });
 
-  worksheet.addRows(formattedUsers);
-  if (!workbook) {
-    throw new CustomError("Fallo al crear el archivo excel", 500);
+    worksheet.getRow(1).eachCell((cell) => {
+      cell.fill = {
+        type: "pattern",
+        pattern: "solid",
+        fgColor: { argb: "FF000000" },
+      };
+      cell.font = { bold: true, color: { argb: "FFFFFFFF" } };
+      cell.border = {
+        top: { style: "thin" },
+        left: { style: "thin" },
+        bottom: { style: "thin" },
+        right: { style: "thin" },
+      };
+    });
+
+    const formattedUsers = users.map((user) => ({
+      ...user,
+      nombre: user.nombre.toLocaleLowerCase(),
+      apellido: user.apellido.toLocaleLowerCase(),
+      segundo_apellido: user.segundo_apellido.toLocaleLowerCase(),
+      email: user.email.toLocaleLowerCase(),
+      dni: user.dni.toLocaleUpperCase(),
+      createdAt: format(user.createdAt, formatedDate, { locale: es }),
+    }));
+
+    worksheet.addRows(formattedUsers);
+
+    await worksheet.protect("tu_contraseña", {
+      objects: true,
+      scenarios: true,
+      selectLockedCells: true,
+      selectUnlockedCells: false,
+      formatCells: true,
+      formatColumns: true,
+      formatRows: true,
+      insertColumns: true,
+      insertRows: true,
+      insertHyperlinks: true,
+      deleteColumns: true,
+      deleteRows: true,
+      sort: true,
+      autoFilter: true,
+      pivotTables: true,
+    });
+
+    return workbook;
+  } catch (error) {
+    throw new CustomError("Fallo al crear el archivo Excel", 500);
   }
-
-  return workbook;
 };
 
 export const excelDto = {
